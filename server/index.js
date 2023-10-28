3fe75e8bed52edea46fc1e05385c95b9413daf46
Nitish Nashine <nitishnashine@gmail.com>
2023-10-28 12:56:58 +0530

    done cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,13 @@ db();
 const app = express();
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://konnect-three.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
